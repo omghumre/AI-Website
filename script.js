1bdce4ffeac6b2883d26df718d765842cdd8fb80
omg12347 <omghumre74@gmail.com>
2024-04-10 23:03:09 +0530

    update

--- script.js
+++ script.js
@@ -17,13 +17,65 @@ $(document).ready(function(){
         0:{
           items:1
         },
-        600:{
-          items:2
-        },
-        1000:{
-          items:3
-        }
+        // 600:{
+        //   items:2
+        // },
+        // 1000:{
+        //   items:3
+        // }
       }
     });
   });
   
+  // script.js
+document.addEventListener('DOMContentLoaded', function() {
+  var cardRow = document.getElementById('cardRow');
+
+  // Data for cards
+  var cardsData = [
+    {
+      title: 'Card 1',
+      content: 'Content for card 1.'
+    },
+    {
+      title: 'Card 2',
+      content: 'Content for card 2.'
+    },
+    {
+      title: 'Card 3',
+      content: 'Content for card 3.'
+    }
+  ];
+
+  // Create cards
+  cardsData.forEach(function(card) {
+    var cardCol = document.createElement('div');
+    cardCol.classList.add('col', 's12', 'm4');
+
+    var cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+
+    var cardImage = document.createElement('div');
+    cardImage.classList.add('card-image', 'waves-effect', 'waves-block', 'waves-light');
+    cardImage.innerHTML = '<img class="activator" src="https://via.placeholder.com/400x200" alt="Card Image">';
+
+    var cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
+    cardContent.innerHTML = '<span class="card-title activator grey-text text-darken-4">' + card.title + '<i class="material-icons right">more_vert</i></span>' +
+                            '<p><a href="#">This is a link</a></p>';
+
+    var cardReveal = document.createElement('div');
+    cardReveal.classList.add('card-reveal');
+    cardReveal.innerHTML = '<span class="card-title grey-text text-darken-4">' + card.title + '<i class="material-icons right">close</i></span>' +
+                            '<p>' + card.content + '</p>';
+
+    cardElement.appendChild(cardImage);
+    cardElement.appendChild(cardContent);
+    cardElement.appendChild(cardReveal);
+    cardCol.appendChild(cardElement);
+    cardRow.appendChild(cardCol);
+  });
+
+  // Initialize Materialize components
+  M.AutoInit();
+});
